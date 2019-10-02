c33effae355bfdcf499d682ef61ee893182b2543
Erin Dawson <erindawson.work@gmail.com>
2019-10-02 16:39:58 -0700

    CSS link styling

--- scripts.js
+++ scripts.js
@@ -5,12 +5,14 @@ const nav3 = document.getElementById("menuItem2")
 const secondParagraph = document.getElementById("secondParagraph");
 const thirdParagraph = document.getElementById("thirdParagraph");
 
-window.addEventListener("load", () => {nav1.classList.add("active");});
+window.addEventListener("load", () => {
+  nav1.classList.add("active");
+});
 
-pageBody.addEventListener('scroll', function(){
-    document.querySelector("#dynamicItem").innerHTML = document.querySelector(".active").innerHTML;
-    document.querySelector("#dynamicItem").style.fontSize = "5.5vh";
-    // console.log(document.getElementsByClassName("active").innerHTML);
+pageBody.addEventListener('scroll', function() {
+  document.querySelector("#dynamicItem").innerHTML = document.querySelector(".active").innerHTML;
+  document.querySelector("#dynamicItem").style.fontSize = "5.5vh";
+  // console.log(document.getElementsByClassName("active").innerHTML);
 })
 
 pageBody.addEventListener('scroll', () => {
@@ -31,44 +33,37 @@ pageBody.addEventListener('scroll', () => {
   }
 });
 
-// function hi(){
-// if(window.matchMedia("(max-width: 350px)").matches){
-//   document.querySelector("#nav").style.display = "none";
-//   pageBody.style.background = "pink";
-// }else {
-//   return
-// }}
-//
-// hi();
 const widthFunk = () =>
-window.addEventListener("resize", function(){
- document.querySelector("#windowWidth").innerHTML = window.innerWidth;}
-)
-
+  window.addEventListener("resize", function() {
+    document.querySelector("#windowWidth").innerHTML = window.innerWidth;
+  })
 widthFunk();
 
 window.addEventListener("resize", function() {
-    if (window.innerWidth < 650) {
-      document.querySelector("#nav").style.display = "none";
-      document.querySelector("#dynamicItem").style.visibility = "visible";
-
-    } else if (window.innerWidth > 650) {
-      document.querySelector("#nav").style.display = "block";
-      document.querySelector("#dynamicItem").style.visibility = "hidden";
-    } else {
-      return
-    }
+  if (window.innerWidth < 650) {
+    document.querySelector("#nav").style.display = "none";
+    document.querySelector("#dynamicItem").style.visibility = "visible";
+  } else if (window.innerWidth > 650) {
+    document.querySelector("#nav").style.display = "grid";
+    document.querySelector("#dynamicItem").style.visibility = "hidden";
+  } else {
+    return
+  }
 });
 
 const collapsedActive = () => {
   if (window.innerWidth < 650) {
     document.querySelector("#nav").style.display = "none";
-    //dynamic equals whatever is collapsedActive
     document.querySelector("#dynamicItem").style.visibility = "visible";
     document.querySelector("#dynamicItem").innerHTML = document.querySelector(".active").innerHTML;
+    //this is where I put that #dynamicItem should always have something, even when a class of .active isn't on anything.
   } else {
     return
   }
 }
-
 collapsedActive();
+
+const collapsedNoScroll = () => {
+  document.querySelector("#dynamicItem").innerHTML = "First"
+}
+collapsedNoScroll();
